58aa011cbc46f4fed53bb1de0605a29894935116
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-11-03 20:42:25 +0530

    small updates

--- app/Http/Middleware/SubdomainConnection.php
+++ app/Http/Middleware/SubdomainConnection.php
@@ -6,7 +6,9 @@ use App\Models\Tenant;
 use Carbon\Carbon;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
 use Symfony\Component\HttpFoundation\Response;
+use DB;
 
 class SubdomainConnection
 {
@@ -21,11 +23,8 @@ class SubdomainConnection
 
         $tenant = Tenant::where('subdomain', $subdomain)->where('status', 'active')->whereDate('expired_on', '>=', Carbon::today())->firstOrFail();
 
-        Config([
-            'database.connections.mysql.database' => $tenant->database,
-            'database.connections.mysql.username' => $tenant->db_user,
-            'database.connections.mysql.password' => $tenant->db_password,
-        ]);
+        Config::set('database.connections.mysql.database', $tenant->database);
+        DB::purge('mysql');
 
         return $next($request);
     }
